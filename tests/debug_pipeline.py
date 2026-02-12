a861e0f5b22025ea8a5b17fb6610da3f68f8060c
abdelrhman yousef mahrous <abdelrhmandeab45@gmail.com>
2026-02-12 21:28:27 +0200

    Delete tests directory

--- tests/debug_pipeline.py
+++
@@ -1,8 +0,0 @@
-from audio.mic import record_until_silence
-from audio.stt import transcribe
-from core.command_router import route_command
-
-record_until_silence()
-text = transcribe("input.wav")
-print("User:", text)
-print("Jarvis:", route_command(text))
